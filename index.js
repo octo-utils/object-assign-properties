46c033355ed409bf63f627f76b2b9380ab983e36
tommyZZM <452490446@qq.com>
2018-05-27 11:21:25 +0800

    style: reindent...

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 const curry = require("fast-curry");
 
 const Symbol = (typeof global.Symbol === "function"
-    && typeof global.Symbol.iterator === "symbol") ? global.Symbol : function(key) {
-        return key+((Math.random()*10).toFixed(2).replace(".","_"));
-    };
+  && typeof global.Symbol.iterator === "symbol") ? global.Symbol : function (key) {
+  return key + ((Math.random() * 10).toFixed(2).replace(".", "_"));
+};
 
 const KEY_ACCESSOR_VALUES = Symbol('accessorValues');
 const KEY_ACCESSOR_VALUE_GETTERS = Symbol('accessorValueGetters');
@@ -15,115 +15,115 @@ const KEY_ACCESSOR_VALUE_SETTERS = Symbol('accessorValueSetters');
 const OptimizedAccessors = {};
 
 function getterOf(prop) {
-    return function () {
-        const self = this;
-        const accessorValues = self[KEY_ACCESSOR_VALUES];
-        const accessorValueGetters = self[KEY_ACCESSOR_VALUE_GETTERS];
+  return function () {
+    const self = this;
+    const accessorValues = self[KEY_ACCESSOR_VALUES];
+    const accessorValueGetters = self[KEY_ACCESSOR_VALUE_GETTERS];
 
-        const lastValue = accessorValues[prop];
-        const getter = accessorValueGetters[prop];
+    const lastValue = accessorValues[prop];
+    const getter = accessorValueGetters[prop];
 
-        return getter(lastValue, prop, self);
-    }
+    return getter(lastValue, prop, self);
+  }
 }
 
 function setterOf(prop) {
-    return function (newValue) {
-        const self = this;
-        const accessorValues = self[KEY_ACCESSOR_VALUES];
-        const accessorValueSetters = self[KEY_ACCESSOR_VALUE_SETTERS];
+  return function (newValue) {
+    const self = this;
+    const accessorValues = self[KEY_ACCESSOR_VALUES];
+    const accessorValueSetters = self[KEY_ACCESSOR_VALUE_SETTERS];
 
-        const setter = accessorValueSetters[prop];
-        let lastValue = accessorValues[prop];
+    const setter = accessorValueSetters[prop];
+    let lastValue = accessorValues[prop];
 
-        lastValue = setter(
-            lastValue, newValue, prop, self
-        );
+    lastValue = setter(
+      lastValue, newValue, prop, self
+    );
 
-        accessorValues[prop] = lastValue;
-    }
+    accessorValues[prop] = lastValue;
+  }
 }
 
 const objectAssignProperties = curry(function (descriptor, properties, object) {
 
-    const isOwnsGetter = typeof descriptor.get === "function";
-    const isOwnsSetter = typeof descriptor.set === "function";
-    const isOwnsGetterOrSetter = isOwnsGetter || isOwnsSetter;
+  const isOwnsGetter = typeof descriptor.get === "function";
+  const isOwnsSetter = typeof descriptor.set === "function";
+  const isOwnsGetterOrSetter = isOwnsGetter || isOwnsSetter;
 
-    const get = descriptor.get;
-    const set = descriptor.set;
+  const get = descriptor.get;
+  const set = descriptor.set;
 
-    const _descriptor = Object.keys(descriptor)
-        .reduce((_descriptor, key) => {
-            if (key === "get" || key === "set") {
-                return _descriptor;
-            }
+  const _descriptor = Object.keys(descriptor)
+    .reduce((_descriptor, key) => {
+      if ( key === "get" || key === "set" ) {
+        return _descriptor;
+      }
 
-            // Cannot both specify accessors and a value or writable attribute
-            if (key === "writable" && isOwnsGetterOrSetter) {
-                return _descriptor;
-            }
+      // Cannot both specify accessors and a value or writable attribute
+      if ( key === "writable" && isOwnsGetterOrSetter ) {
+        return _descriptor;
+      }
 
-            _descriptor[key] = descriptor[key];
-            return _descriptor;
-    }, { });
+      _descriptor[key] = descriptor[key];
+      return _descriptor;
+    }, {});
 
-    if (isOwnsGetterOrSetter) {
-        [
-            KEY_ACCESSOR_VALUES,
-            KEY_ACCESSOR_VALUE_GETTERS,
-            KEY_ACCESSOR_VALUE_SETTERS
-        ].forEach(key => {
-                let descriptor = Object.getOwnPropertyDescriptor(
-                    object, key
-                );
+  if ( isOwnsGetterOrSetter ) {
+    [
+      KEY_ACCESSOR_VALUES,
+      KEY_ACCESSOR_VALUE_GETTERS,
+      KEY_ACCESSOR_VALUE_SETTERS
+    ].forEach(key => {
+      let descriptor = Object.getOwnPropertyDescriptor(
+        object, key
+      );
 
-                if (!descriptor) {
-                    Object.defineProperty(object, key, {
-                        value:{},
-                        enumerable:false,
-                        configurable:false
-                    })
-                }
-            });
-    }
+      if ( !descriptor ) {
+        Object.defineProperty(object, key, {
+          value: {},
+          enumerable: false,
+          configurable: false
+        })
+      }
+    });
+  }
 
-    const accessorValues = object[KEY_ACCESSOR_VALUES];
-    const accessorValueGetters = object[KEY_ACCESSOR_VALUE_GETTERS];
-    const accessorValueSetters = object[KEY_ACCESSOR_VALUE_SETTERS];
-    
-    return Object.defineProperties(object, Object.keys(properties)
-        .reduce((_properties, prop) => {
-            let value = properties[prop];
-            
-            if (isOwnsGetterOrSetter) {
-                accessorValues[prop] = value;
-                accessorValueGetters[prop] = get;
-                accessorValueSetters[prop] = set;
-            }
+  const accessorValues = object[KEY_ACCESSOR_VALUES];
+  const accessorValueGetters = object[KEY_ACCESSOR_VALUE_GETTERS];
+  const accessorValueSetters = object[KEY_ACCESSOR_VALUE_SETTERS];
 
-            let optimizedAccessor = OptimizedAccessors[prop];
+  return Object.defineProperties(object, Object.keys(properties)
+    .reduce((_properties, prop) => {
+      let value = properties[prop];
 
-            if (!optimizedAccessor) {
-                optimizedAccessor = [
-                    getterOf(prop), setterOf(prop)
-                ];
-                OptimizedAccessors[prop] = optimizedAccessor;
-            }
+      if ( isOwnsGetterOrSetter ) {
+        accessorValues[prop] = value;
+        accessorValueGetters[prop] = get;
+        accessorValueSetters[prop] = set;
+      }
 
-            let _descriptorValue = isOwnsGetterOrSetter ? Object.assign(
-                isOwnsGetter ? {
-                    get: optimizedAccessor[0]
-                } : {},
-                isOwnsSetter ? {
-                    set: optimizedAccessor[1]
-                } : {}
-            ) : { value };
+      let optimizedAccessor = OptimizedAccessors[prop];
 
-            _properties[prop] = Object.assign(
-                {}, _descriptor, _descriptorValue
-            );
-            return _properties
+      if ( !optimizedAccessor ) {
+        optimizedAccessor = [
+          getterOf(prop), setterOf(prop)
+        ];
+        OptimizedAccessors[prop] = optimizedAccessor;
+      }
+
+      let _descriptorValue = isOwnsGetterOrSetter ? Object.assign(
+        isOwnsGetter ? {
+          get: optimizedAccessor[0]
+        } : {},
+        isOwnsSetter ? {
+          set: optimizedAccessor[1]
+        } : {}
+      ) : { value };
+
+      _properties[prop] = Object.assign(
+        {}, _descriptor, _descriptorValue
+      );
+      return _properties
     }, {}));
 })
 
